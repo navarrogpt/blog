bd1bca8c8e432b0296de72542f4ff0205c2339cd
Patricia Navarro <navarrogpt@gmail.com>
2018-07-25 19:38:17 +0900

    added crud functionalities

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ApplicationRecord
-	validates_presence_of :title
-	validates_presence_of :content
+  
+  after_create :set_publish_date
+
+  validates_presence_of :title
+  validates_presence_of :content
+
+  def set_publish_date
+    self.publish_date = self.created_at
+    save
+  end
 end
